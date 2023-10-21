e77f05d65edf01ef715f82e4a12b4bc32da59ef5
Ne0-exe <ne0exe@protonmail.com>
2023-10-21 20:03:36 +0200

    Add logout

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+from django.contrib import messages
 
 from item.models import Category, Item
 
@@ -30,3 +32,8 @@ def signup(request):
     return render(request, 'core/signup.html', {
         'form': form
     })
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("You Were Logged Out."))
+    return redirect('/')
